2ea45a7632e5c51193fd323f084e8b3113f4992d
herbig <mikeherbig@gmail.com>
2023-08-30 23:38:25 -0400

    web3 auth stuff in progress

--- src/constants.tsx
+++ src/constants.tsx
@@ -5,3 +5,7 @@
 
 export const PROVIDER = 'https://rpc.ankr.com/eth_goerli';
 export const CHAIN_ID = 5;
+// TODO whitelist deployed URL https://dashboard.web3auth.io/home/web3auth
+export const WEB3_AUTH_CLIENT_ID = 'BIr57Q4Fdt7dmJVrRgkW5bUTbjRV7sxJamqChw4hxEUFrMRU57F9sLwSnutEqFZLk1mnQ4krJvRzvVFTdMuoMoc';
+export const AUTH0_DOMAIN = 'https://dev-4elqbiwwyzsv3a4v.us.auth0.com';
+export const AUTH0_CLIENT_ID = '6FOSoWHjCH2tHZWppCUT2w35MKt5v7hB';
