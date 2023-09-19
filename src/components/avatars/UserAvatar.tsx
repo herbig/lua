216362acabcf8ad6f22a6a73abe4e27dc21fa674
herbig <mikeherbig@gmail.com>
2023-09-19 13:27:22 -0400

    UI tweaks

--- src/components/avatars/UserAvatar.tsx
+++ src/components/avatars/UserAvatar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { AvatarProps, Avatar } from '@chakra-ui/react';
+import { AvatarProps, Avatar, useColorModeValue } from '@chakra-ui/react';
 import { useGetUserValue } from '../../utils/users';
 
 interface Props extends Omit<AvatarProps, 'src'> {
@@ -8,10 +8,11 @@ interface Props extends Omit<AvatarProps, 'src'> {
 
 export function UserAvatar({ address, ...props }: Props) {
   const imageUri = useGetUserValue(address, 'avatar_img');
+  const bg = useColorModeValue('gray.400', 'gray.500');
   // backgroundColor is set to avoid a weird bug
   // where the bg color disappears after the history
   // list is updated...
   return (
-    <Avatar {...props} backgroundColor='gray.400' ignoreFallback={true} src={imageUri ? imageUri : undefined} />
+    <Avatar {...props} backgroundColor={bg} ignoreFallback={true} src={imageUri ? imageUri : undefined} />
   );
 }
--- src/components/custom/NumberPad.tsx
+++ src/components/custom/NumberPad.tsx
@@ -98,9 +98,9 @@ export function NumberPad({ accountMax, amount, setAmount, ...props }: Props) {
     <Flex flexDirection='column' {...props} >
       <Center flex='1'>
         <Flex flexDirection="column">
-          <HStack alignSelf='center' ms='-2rem' mb='-1rem'>
-            <Text fontSize='4xl' as='b'>$</Text>
-            <Text fontSize={amountString.length > 5 ? '7xl' : '8xl'} as='b'>{amountString}</Text>
+          <HStack alignSelf='center' minH='9rem' ms='-2rem' mb='-1rem'>
+            <Text fontSize={amountString.length > 6 ? '3xl' : '4xl'} as='b'>$</Text>
+            <Text fontSize={amountString.length > 6 ? '7xl' : '8xl'} as='b'>{amountString}</Text>
           </HStack>
           <Box alignSelf='center' ms='-0.5rem'>
             <Button borderRadius='2rem' variant="ghost" size='md' onClick={() => {
